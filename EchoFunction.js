57e80c5586f290764db7c73efc01b636b62c8fe5
Petya Zhekova <71344477+petruna93@users.noreply.github.com>
2021-09-16 23:27:38 +0300

    Syntax, Functions and Statements - Lab

---
+++ EchoFunction.js
@@ -0,0 +1,6 @@
+function printText(input) {
+    console.log(input.length);
+    console.log(input);
+}
+
+printText('gtgt');
---
+++ StringLength.js
@@ -0,0 +1,8 @@
+function result(a, b, c) {
+    let sum = a.length + b.length + c.length;
+    console.log(sum);
+    console.log(Math.floor(sum / 3));
+}
+
+result('pasta', '5', '22.3');
+result('chocolate', 'ice cream', 'cake');
