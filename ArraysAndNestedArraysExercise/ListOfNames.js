8de91b5bc9232c14e2d24c9b0f9b10a924f004ca
Petya Zhekova <71344477+petruna93@users.noreply.github.com>
2021-09-28 21:46:13 +0300

    Arrays and Nested Arrays - Exercise

---
+++ ArraysAndNestedArraysExercise/ListOfNames.js
@@ -0,0 +1,10 @@
+function solve(a) {
+    let count = 1;
+    a.sort((a, b) => a.localeCompare(b));
+    for (let i = 0; i < a.length; i++) {
+        let name = a[i];
+        console.log(`${count}.${name}`);
+        count++;
+    }
+}
+solve(["John", "john", "Christina", "Ema"]);
