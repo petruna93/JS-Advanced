868352029676fa766c4bda581d3d949bd2661cf0
Petya Zhekova <71344477+petruna93@users.noreply.github.com>
2021-09-28 19:59:39 +0300

    Arrays and Nested Arrays - Exercise

---
+++ ArraysAndNestedArraysExercise/PrintAnArrayWithAGivenDelimiter.js
@@ -0,0 +1,21 @@
+function solve(a, b) {
+    console.log(a.join(b));
+}
+
+solve(['One',
+        'Two',
+        'Three',
+        'Four',
+        'Five'
+    ],
+    '-'
+);
+solve(['How about no?',
+        'I',
+        'will',
+        'not',
+        'do',
+        'it!'
+    ],
+    '_'
+);
