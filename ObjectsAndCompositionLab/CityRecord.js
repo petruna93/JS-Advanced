39000ddaa01b182844696eb92bb8af2d24483709
Petya Zhekova <71344477+petruna93@users.noreply.github.com>
2021-10-02 12:42:37 +0300

    Objects and Composition - Lab

---
+++ ObjectsAndCompositionLab/CityRecord.js
@@ -0,0 +1,15 @@
+function cityRecord(name, population, treasury) {
+    let city = { name, population, treasury };
+    console.log(city);
+    return city;
+
+}
+
+cityRecord('Tortuga',
+    7000,
+    15000
+);
+cityRecord('Santo Domingo',
+    12000,
+    23500
+);
