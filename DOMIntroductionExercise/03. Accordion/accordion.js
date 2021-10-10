ee9f984711b84754a22f8eeb507158b0d05767af
Petya Zhekova <71344477+petruna93@users.noreply.github.com>
2021-10-10 21:39:47 +0300

    DOM Introduction - Exercise

---
+++ DOMIntroductionExercise/03. Accordion/accordion.js
@@ -0,0 +1,14 @@
+function toggle() {
+    let button = document.querySelector(".button");
+    let textDIV = document.getElementById("extra");
+
+    button.textContent = button.textContent ==
+        "More" ? "Less" : "More";
+
+    textDIV.style.display =
+        textDIV.style.display == "none" ||
+        textDIV.style.display == "" ?
+        textDIV.style.display = "block" :
+        textDIV.style.display = "none";
+
+}
